04c37e7c5c6e0dca90228ea2fc0d374b2d53dd18
Paula Dumrauf <dumraufp@gmail.com>
2023-06-06 14:06:11 +0200

    include ids and router init

---
+++ src/views/VideoWatchView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Hi, let's watch a video</h1>
+    <h3>{{ this.$route.params.id }}</h3>
+    {{ video.name }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'VideoWatchView',
+    computed: {
+      video() {
+        return this.$store.state.videos.find(video => video.name === this.$store.state.id)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
